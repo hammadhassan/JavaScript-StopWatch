c2828784cff4ee4d16c600ca4dcdf67ef579825e
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-11 01:30:06 +0500

    counter works

---
+++ app.js
@@ -0,0 +1,37 @@
+var sec = 00;
+var min = 00;
+var hour = 00;
+
+function time() {
+    if (sec >= 60) {
+        sec = 00;
+        min++;
+        if (min >= 60) {
+            min = 00;
+            hour++;
+        }
+    }
+    document.getElementById("timer").innerHTML = (hour + ":" + min + ":" + sec++ );
+    timer();
+}
+
+function timer() {
+    setTimeout(time, 1000);
+} 
+
+start.onclick = timer;
+ document.getElementById("start").disabled;
+if (time > 1) {
+ 	//document.getElementsByID("start").disabled = true;
+     //document.getElementById("start").disabled = true;
+     
+}
+
+stop.onclick = function() {
+     clearTimeout(timer);
+}
+
+reset.onclick = function() {
+     document.getElementsByTagName("h2") = "00:00:00";
+     sec = 00; min = 00; hour = 00; 
+    }

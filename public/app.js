2bcb411c2a2023cfbd44819d3c5686950eb285aa
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-11-13 17:00:46 +0500

    apply disable function

--- public/app.js
+++ public/app.js
@@ -17,20 +17,30 @@ function time() {
     document.getElementById("timer").innerHTML = (hour + ":" + min + ":" + sec++ );
     timer();
 }
- 
-start.onclick = timer;
-// if (timer >= 1) {
-//  	document.getElementByID("start").disabled = true;  
-// }
+// document.getElementById("start").disabled = true;
+ start.onclick = timer;
+ if (document.getElementById("timer").value.length >= 1) {
+    document.getElementById("start").disabled = true;
+}
 function stop() {
      clearTimeout(t);
 }
 stop.onclick = stop;
+if (document.getElementById("timer").value.length == 0) {
+document.getElementById("stop").disabled = true;
+} else {
+    document.getElementById("stop").disabled = false;
+}
 
 function reset() {
      clearInterval(t);
      document.getElementById("timer").innerHTML = "00:00:00";
-     sec = 00; min = 00; hour = 00;
-     
+     sec = 00; min = 00; hour = 00;  
 }
 reset.onclick = reset;
+
+if (document.getElementById("timer").value.length == 0) {
+document.getElementById("reset").disabled = true;
+} else {
+    document.getElementById("reset").disabled = false;
+}
